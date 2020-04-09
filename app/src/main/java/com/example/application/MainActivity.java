908e380902190a66ba92d38d348e8ec6a8705d01
MdDoyalBabu <mddoyalbabu2020>
2020-04-09 19:44:50 +0600

    Addeda Toast

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show();
+
     }
 }
